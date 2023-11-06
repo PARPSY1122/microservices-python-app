ed01e06d239b2274bac78096a8c9db9f382c66a4
PARPSY1122 <119403673+PARPSY1122@users.noreply.github.com>
2023-11-06 14:04:42 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "s3-bucket" # Replace with your actual S3 bucket name
+    bucket = "reddy-ajay12321" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1" #change your region
+    region = "us-east-2" #change your region
   }
 }
